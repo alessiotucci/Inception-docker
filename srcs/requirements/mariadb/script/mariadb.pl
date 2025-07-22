fe86ee6d8a118be55972d87927ddb5f01f73a3f1
atucci <alessiotucci33@gmail.com>
2025-07-22 20:47:46 +0200

    push after a long day with osema

--- srcs/requirements/mariadb/script/mariadb.pl
+++ srcs/requirements/mariadb/script/mariadb.pl
@@ -1,1 +1,84 @@
 #!/usr/bin/perl
+use strict;
+use warnings;
+use IO::Socket::INET;
+
+# Print ASCII
+my $ascii = '       ▘  ▄ ▄ 
+▛▛▌▀▌▛▘▌▀▌▌▌▙▘
+▌▌▌█▌▌ ▌█▌▙▘▙▘
+              ';
+print("\n$ascii\n");
+
+# Read secret file
+sub read_secret {
+    my ($file) = @_;
+    open my $fh, '<', $file or die "MARIADB ERROR: Cannot open secret $file: $!";
+    my $val = <$fh>;
+    close $fh;
+    chomp $val;
+    return $val;
+}
+
+# Read secrets
+my $root_pass = read_secret("/run/secrets/db_root_password.txt");
+my $db_name   = "wordpress"; # static name as per subject
+my $user      = read_secret("/run/secrets/db_user.txt");
+my $pass      = read_secret("/run/secrets/db_user_password.txt");
+
+# Start mysqld_safe in background
+print "MARIADB: Starting mysqld_safe...\n";
+my $pid = fork();
+if ($pid == 0) {
+    exec("mysqld_safe");
+    exit(1); # If exec fails
+}
+
+# Wait for MySQL to be ready on 127.0.0.1:3306
+print "MARIADB: Waiting for MySQL on 127.0.0.1:3306...\n";
+my $ready = 0;
+for my $try (1..30) {
+    my $sock = IO::Socket::INET->new(PeerAddr => "127.0.0.1", PeerPort => 3306, Proto => 'tcp', Timeout => 2);
+    if ($sock) {
+        $ready = 1;
+        close($sock);
+        last;
+    }
+    print "MARIADB: Try $try/30 failed; retry in 2s\n";
+    sleep 2;
+}
+
+die "MARIADB FATAL: MySQL never came up\n" unless $ready;
+
+print "MARIADB: Connected. Running initialization SQL...\n";
+
+# Prepare and run SQL commands
+my $sql = qq{
+    CREATE DATABASE IF NOT EXISTS $db_name;
+    CREATE USER IF NOT EXISTS '$user'\@'%' IDENTIFIED BY '$pass';
+    GRANT ALL PRIVILEGES ON $db_name.* TO '$user'\@'%';
+    FLUSH PRIVILEGES;
+};
+
+system("mysql", "-uroot", "-p$root_pass", "-e", $sql) == 0
+    or die "MARIADB ERROR: Failed to run initialization SQL\n";
+
+print "MARIADB: Initialization complete. Handing over to mysqld_safe...\n";
+
+#osema debug
+#exec("/etc/init.d/mariadb restart");
+
+# Wait for mysqld_safe (PID 1) to stay alive
+waitpid($pid, 0);
+#
+#my $ascii = '       ▘  ▄ ▄ 
+#▛▛▌▀▌▛▘▌▀▌▌▌▙▘
+#▌▌▌█▌▌ ▌█▌▙▘▙▘
+#              ';
+#
+#
+#print("\n$ascii\n");
+#exec(@ARGV);
+#!/usr/bin/perl
+
+
--- srcs/requirements/wordpress/script/wordpress.pl
+++ srcs/requirements/wordpress/script/wordpress.pl
@@ -1,2 +1,78 @@
 #!/usr/bin/perl
+use strict;
+use warnings;
+use IO::Socket::INET;
 
+sub read_secret {
+    my ($file) = @_;
+    return undef unless $file;
+    open my $fh, '<', $file or die "WORDPRESS ERROR: Cannot open $file: $!";
+    my $value = <$fh>;
+    close $fh;
+    chomp $value;
+    return $value;
+}
+
+print "WORDPRESS: Starting configuration...\n";
+
+# Get env variables
+my $db_host = $ENV{'WORDPRESS_DB_HOST'} or die "WORDPRESS ERROR: WORDPRESS_DB_HOST not set";
+my $db_name = $ENV{'WORDPRESS_DB_NAME'} or die "WORDPRESS ERROR: WORDPRESS_DB_NAME not set";
+my $user_file = $ENV{'WORDPRESS_DB_USER_FILE'} or die "WORDPRESS ERROR: Secret file path not provided";
+my $pass_file = $ENV{'WORDPRESS_DB_PASSWORD_FILE'} or die "WORDPRESS ERROR: Secret file path not provided";
+
+# Read secrets
+my $db_user = read_secret($user_file);
+my $db_pass = read_secret($pass_file);
+
+# Wait for DB
+print "WORDPRESS: Waiting for DB ($db_host)...\n";
+my ($host, $port) = split(':', $db_host);
+$port ||= 3306;
+
+for my $try (1..60)
+{
+    my $sock = IO::Socket::INET->new(PeerAddr => $host, PeerPort => $port, Proto => 'tcp', Timeout => 2);
+    if ($sock)
+	{
+        close($sock);
+        print "WORDPRESS: DB is up!\n";
+        last;
+    }
+    print "WORDPRESS: DB not ready ($try/60), retrying...\n";
+    sleep 2;
+}
+
+
+## Generate wp-config.php if not exists
+my $config = "/var/www/html/wp-config.php";
+    print "WORDPRESS: Creating wp-config.php\n";
+
+    open my $in, "<", "/var/www/html/wp-config-sample.php" or die "Can't read sample config: $!";
+    my $content = do { local $/; <$in> };
+    close $in;
+
+    $content =~ s/database_name_here/$db_name/g;
+    $content =~ s/username_here/$db_user/g;
+    $content =~ s/password_here/$db_pass/g;
+    $content =~ s/localhost/$host/g;
+
+    # Get unique salts
+    my $salts = `curl -s https://api.wordpress.org/secret-key/1.1/salt/`;
+    $content =~ s/\/\*\*#@\+.*?#@-\*\//$salts/s;
+
+    open my $out, ">", $config or die "Can't write config: $!";
+    print $out $content;
+    close $out;
+
+my $var = '
+▗▖ ▗▖ ▗▄▖ ▗▄▄▖ ▗▄▄▄ ▗▄▄▖ ▗▄▄▖ ▗▄▄▄▖ ▗▄▄▖ ▗▄▄▖
+▐▌ ▐▌▐▌ ▐▌▐▌ ▐▌▐▌  █▐▌ ▐▌▐▌ ▐▌▐▌   ▐▌   ▐▌
+▐▌ ▐▌▐▌ ▐▌▐▛▀▚▖▐▌  █▐▛▀▘ ▐▛▀▚▖▐▛▀▀▘ ▝▀▚▖ ▝▀▚▖
+▐▙█▟▌▝▚▄▞▘▐▌ ▐▌▐▙▄▄▀▐▌   ▐▌ ▐▌▐▙▄▄▖▗▄▄▞▘▗▄▄▞▘
+
+
+
+';
+print ("\n$var\n");
+exec(@ARGV);
